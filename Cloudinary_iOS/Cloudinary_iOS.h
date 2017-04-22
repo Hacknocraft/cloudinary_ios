11b6f3d4b32dd8304c942dd4d7cb7fcb29f98cb2
wanghao <hao.wang@hacknocraft.com>
2017-04-22 03:54:34 -0400

    added dynamic framework

---
+++ Cloudinary_iOS/Cloudinary_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Cloudinary_iOS.h
+//  Cloudinary_iOS
+//
+//  Created by HAO WANG on 4/22/17.
+//  Copyright © 2017 Cloudinary Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cloudinary_iOS.
+FOUNDATION_EXPORT double Cloudinary_iOSVersionNumber;
+
+//! Project version string for Cloudinary_iOS.
+FOUNDATION_EXPORT const unsigned char Cloudinary_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cloudinary_iOS/PublicHeader.h>
+
+
